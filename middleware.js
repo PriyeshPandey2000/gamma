9083c88f7fed1dfbf38edb46841d9ee81e2361d3
PriyeshPandey2000 <priyeshpandey2000@gmail.com>
2024-04-07 21:38:21 +0530

    .

--- middleware.js
+++ middleware.js
@@ -1,27 +1,27 @@
-import { NextResponse } from 'next/server';
+// import { NextResponse } from 'next/server';
 
-export function middleware(request) {
-  const path = request.nextUrl.pathname;
+// export function middleware(request) {
+//   const path = request.nextUrl.pathname;
 
-  const isPublicPath = path === '/login' || path === '/signup' || path === '/verifyemail';
+//   const isPublicPath = path === '/login' || path === '/signup' || path === '/verifyemail';
 
-  const token = request.cookies.get('token')?.value || '';
+//   const token = request.cookies.get('token')?.value || '';
 
-  if (isPublicPath && token) {
-    return NextResponse.redirect(new URL('/', request.nextUrl));
-  }
+//   if (isPublicPath && token) {
+//     return NextResponse.redirect(new URL('/', request.nextUrl));
+//   }
 
-  if (!isPublicPath && !token) {
-    return NextResponse.redirect(new URL('/login', request.nextUrl));
-  }
-}
+//   if (!isPublicPath && !token) {
+//     return NextResponse.redirect(new URL('/login', request.nextUrl));
+//   }
+// }
 
-export const config = {
-  matcher: [
-    '/',
-    '/profile',
-    '/login',
-    '/signup',
-    '/verifyemail'
-  ]
-};
+// export const config = {
+//   matcher: [
+//     '/',
+//     '/profile',
+//     '/login',
+//     '/signup',
+//     '/verifyemail'
+//   ]
+// };
