962dacf82464abd8d47f38870a604ff2b4053850
PriyeshPandey2000 <priyeshpandey2000@gmail.com>
2024-04-07 21:32:07 +0530

    .

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,10 +2,25 @@
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Login from '@/pages/Login';
+import Cookies from 'js-cookie';
 
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // Check if token is present in cookies
+    const token = Cookies.get('token') || '';
+    setIsLoggedIn(!!token);
+  }, [Cookies.get('token')]);
+  
+
+  const handleLogout = () => {
+    // Perform logout logic here (e.g., clear authentication token)
+    // Redirect the user to logout endpoint or clear the token from cookies
+    setIsLoggedIn(false); // Update the login state to false
+  };
 
   useEffect(() => {
     const handleResize = () => {
@@ -67,13 +82,19 @@ const Navbar = () => {
 
         {/* Login and Signup buttons */}
         <div className={`flex items-center space-x-4 ${isMobile ? 'ml-auto' : ''}`}>
-        <Link href="/Login">
-            <button className="border border-custom-blue text-blue-900 px-3 py-1 rounded-full text-sm hover:bg-blue-100">Login</button>
-          </Link>
-          <Link href="/Signup">
-  <button className={`bg-custom-blue text-white px-4 py-1 rounded-full text-sm hover:bg-blue-800 ${isMobile ? 'w-18 ' : 'mr-4'}`}>Sign Up</button>
-  </Link>
-</div>
+          {isLoggedIn ? ( // If user is logged in
+            <button onClick={handleLogout} className="border border-custom-blue text-blue-900 px-3 py-1 rounded-full text-sm hover:bg-blue-100">Logout</button>
+          ) : ( // If user is not logged in
+            <>
+              <Link href="/Login">
+                <button className="border border-custom-blue text-blue-900 px-3 py-1 rounded-full text-sm hover:bg-blue-100">Login</button>
+              </Link>
+              <Link href="/Signup">
+                <button className={`bg-custom-blue text-white px-4 py-1 rounded-full text-sm hover:bg-blue-800 ${isMobile ? 'w-18 ' : 'mr-4'}`}>Sign Up</button>
+              </Link>
+            </>
+          )}
+        </div>
       </div>
     </nav>
   );
