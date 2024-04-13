563268e91d5538708ce0e83531973137de8488e3
PriyeshPandey2000 <priyeshpandey2000@gmail.com>
2024-04-13 21:57:46 +0530

    .

--- pages/courses/course3.js
+++ pages/courses/course3.js
@@ -1,48 +1,48 @@
-import { FollowUs } from '@/components/FollowUs'
-import App from '@/components/LoginNav'
-import Navbar from '@/components/Navbar'
-import Benefit from '@/components/SingleCourse/Benefits'
-import Other from '@/components/SingleCourse/OtherCourse'
-import Require from '@/components/SingleCourse/Requirements'
-import SBanner from '@/components/SingleCourse/SBanner'
-import StudentsPlaced from '@/components/StudentsPlaced'
-import React from 'react'
-import "tailwindcss/tailwind.css"
+// import { FollowUs } from '@/components/FollowUs'
+// import App from '@/components/LoginNav'
+// import Navbar from '@/components/Navbar'
+// import Benefit from '@/components/SingleCourse/Benefits'
+// import Other from '@/components/SingleCourse/OtherCourse'
+// import Require from '@/components/SingleCourse/Requirements'
+// import SBanner from '@/components/SingleCourse/SBanner'
+// import StudentsPlaced from '@/components/StudentsPlaced'
+// import React from 'react'
+// import "tailwindcss/tailwind.css"
 
-function SingleCourse() {
-    const courseId = "6611bf77467c3ba9ab016957";
-  return (
-    <div>
-        <App/>
+// function SingleCourse() {
+//     const courseId = "6611bf77467c3ba9ab016957";
+//   return (
+//     <div>
+//         <App/>
       
-        <SBanner
-  title="Complete App Development
-  "
-  description="You will learn cross-platform development
-  Includes Flutter with MERN/Java
-  With deployment on the play store
-  Includes Live Projects
+//         <SBanner
+//   title="Complete App Development
+//   "
+//   description="You will learn cross-platform development
+//   Includes Flutter with MERN/Java
+//   With deployment on the play store
+//   Includes Live Projects
   
-  "
-  reviewRating="4.0"
-  reviewCount={230}
-  studentsPlaced={329}
-  courseId={courseId}
-/>
-        <div className="justify-start">
-        <div className="container md:ml-10 max-w-[850px]  xs:ml-0 ">
-        <Benefit />
-        </div>
-        </div>
-        <Require/>
+//   "
+//   reviewRating="4.0"
+//   reviewCount={230}
+//   studentsPlaced={329}
+//   courseId={courseId}
+// />
+//         <div className="justify-start">
+//         <div className="container md:ml-10 max-w-[850px]  xs:ml-0 ">
+//         <Benefit />
+//         </div>
+//         </div>
+//         <Require/>
        
-        <StudentsPlaced/>
-        <div className="container mx-auto max-w-[1200px]  ">
-        <Other/>
-        </div>
-        <FollowUs/>
-        </div>
-  )
-}
+//         <StudentsPlaced/>
+//         <div className="container mx-auto max-w-[1200px]  ">
+//         <Other/>
+//         </div>
+//         <FollowUs/>
+//         </div>
+//   )
+// }
 
-export default SingleCourse
+// export default SingleCourse
--- pages/courses/course6.js
+++ pages/courses/course6.js
@@ -1,47 +1,47 @@
-import { FollowUs } from '@/components/FollowUs'
-import App from '@/components/LoginNav'
-import Navbar from '@/components/Navbar'
-import Benefit from '@/components/SingleCourse/Benefits'
-import Other from '@/components/SingleCourse/OtherCourse'
-import Require from '@/components/SingleCourse/Requirements'
-import SBanner from '@/components/SingleCourse/SBanner'
-import SBanner2 from '@/components/SingleCourse/SBanner2'
-import StudentsPlaced from '@/components/StudentsPlaced'
-import React from 'react'
-import "tailwindcss/tailwind.css"
+// import { FollowUs } from '@/components/FollowUs'
+// import App from '@/components/LoginNav'
+// import Navbar from '@/components/Navbar'
+// import Benefit from '@/components/SingleCourse/Benefits'
+// import Other from '@/components/SingleCourse/OtherCourse'
+// import Require from '@/components/SingleCourse/Requirements'
+// import SBanner from '@/components/SingleCourse/SBanner'
+// import SBanner2 from '@/components/SingleCourse/SBanner2'
+// import StudentsPlaced from '@/components/StudentsPlaced'
+// import React from 'react'
+// import "tailwindcss/tailwind.css"
 
-function SingleCourse() {
-    const courseId = "6611bfe0467c3ba9ab01695d";
-  return (
-    <div>
-        <App/>
+// function SingleCourse() {
+//     const courseId = "6611bfe0467c3ba9ab01695d";
+//   return (
+//     <div>
+//         <App/>
       
-        <SBanner2
-  title="Complete Java Development with Spring Boot
-  "
-  description="Learn Java Basics to Advanced concepts
-  Including DSA and Development
-  Includes Live Projects
-  "
-  reviewRating="4.0"
-  reviewCount={230}
-  studentsPlaced={329}
-  courseId={courseId}
-/>
-        <div className="justify-start">
-        <div className="container md:ml-10 max-w-[850px]  xs:ml-0 ">
-        <Benefit />
-        </div>
-        </div>
-        <Require/>
+//         <SBanner2
+//   title="Complete Java Development with Spring Boot
+//   "
+//   description="Learn Java Basics to Advanced concepts
+//   Including DSA and Development
+//   Includes Live Projects
+//   "
+//   reviewRating="4.0"
+//   reviewCount={230}
+//   studentsPlaced={329}
+//   courseId={courseId}
+// />
+//         <div className="justify-start">
+//         <div className="container md:ml-10 max-w-[850px]  xs:ml-0 ">
+//         <Benefit />
+//         </div>
+//         </div>
+//         <Require/>
        
-        <StudentsPlaced/>
-        <div className="container mx-auto max-w-[1200px]  ">
-        <Other/>
-        </div>
-        <FollowUs/>
-        </div>
-  )
-}
+//         <StudentsPlaced/>
+//         <div className="container mx-auto max-w-[1200px]  ">
+//         <Other/>
+//         </div>
+//         <FollowUs/>
+//         </div>
+//   )
+// }
 
-export default SingleCourse
+// export default SingleCourse
