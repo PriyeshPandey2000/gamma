e6f02f6fdc5f962aee23956dc45fe8edba1c5e21
PriyeshPandey2000 <priyeshpandey2000@gmail.com>
2024-04-15 03:47:08 +0530

    .

--- pages/Navbar.js
+++ pages/Navbar.js
@@ -8,7 +8,8 @@ import axios from 'axios';
 // import { isLoggedIn } from '@/app/helpers/auth';
 // import { isLoggedIn } from '@/app/helpers/auth';
 // import { useAuth } from '@/contexts/AuthContext';
-import { useAuthStore } from '@/stores/authStore';
+
+import useAuthStore from '@/stores/authStore';
 
 const Navbar = ( ) => {
     // const { isLoggedIn, logout ,login} = useAuth();
