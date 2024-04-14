66070be0a5310b6da78d5913131812d6169919db
PriyeshPandey2000 <priyeshpandey2000@gmail.com>
2024-04-14 17:51:42 +0530

    .

--- components/DashGrid.js
+++ components/DashGrid.js
@@ -3,9 +3,9 @@ import * as React from "react";
 function DashGrid({ title, description }) {
   return (
     <div className="flex flex-col">
-      <div className="px-5 w-full max-md:max-w-full">
+      <div className="px-5 w-full max-md:max-w-full mt-7">
         <div className="flex gap-5 max-md:flex-col max-md:gap-0">
-          <div className="flex flex-col w-[33%] max-md:ml-0 max-md:w-full">
+          <div className="flex flex-col w-[33%] mx-auto max-md:w-full">
             <div className="flex flex-col grow px-5 py-6 w-full text-base rounded-2xl border border-solid shadow-sm border-zinc-300 max-md:mt-9 max-md:max-w-full">
               <img
                 loading="lazy"
