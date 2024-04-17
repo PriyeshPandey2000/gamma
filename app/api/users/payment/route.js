9edb14c80dc5e841ad050d7eb73e443145c6b828
PriyeshPandey2000 <priyeshpandey2000@gmail.com>
2024-04-17 13:12:00 +0530

    .

--- app/api/users/payment/route.js
+++ app/api/users/payment/route.js
@@ -73,9 +73,9 @@ export async function POST(request) {
           accept: "application/json",
           "Content-Type": "application/json",
           "X-VERIFY": checksum,
-          "Access-Control-Allow-Origin": "https://mercury.phonepe.com",
+          "Access-Control-Allow-Origin": "https://www.gammaprep.in",
           "Access-Control-Allow-Methods": "POST",
-          "Access-Control-Allow-Headers": "Content-Type",
+          "Access-Control-Allow-Headers": "X-Requested-With,content-type,x-app-id, x-auth-token, id-mercury",
           
         },
       }
